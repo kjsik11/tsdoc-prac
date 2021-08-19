442db37d98f4374111345e9aff4c79f888fd935e
jongsik <kjsik11@gmail.com>
2021-08-19 15:18:40 +0900

    d

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,17 +2,12 @@ module.exports = {
   apps: [
     {
       name: 'tsdoc',
-      script: 'yarn',
+      script: 'npm',
       args: 'start',
       cwd: '/home/ec2-user/Programming/tsdoc-prac',
       instances: 2,
       autorestart: true,
-      watch: false,
-      env_production: {
-        NODE_ENV: 'production',
-        API_URL: 'YOUR ENV URL',
-        PORT: 3000,
-      },
+      watch: true,
     },
   ],
 };
